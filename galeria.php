7a5f55a77cda45aa5ec1afd3ce38bf3e3a39939b
Goncal01 <75909074+Goncal01@users.noreply.github.com>
2021-01-07 19:56:42 +0000

    update galeria.php

--- galeria.php
+++ galeria.php
@@ -75,7 +75,9 @@
                         <img alt="" src="images/31.jpg">
                         <img alt="" src="images/32.jpg">
                         <img alt="" class="span-2" src="images/33.jpg">
-                        <img alt="" src="images/34.jpg">
+						<img alt="" src="images/33.jpg">
+						<img alt="" src="images/33.jpg">
+						
                     </div>
                 </div>
             </section>
@@ -94,7 +96,7 @@
 										
 											<!-- Break -->
 											<div class="col-12">
-												<textarea name="textarea" id="textarea" placeholder="Alpha beta gamma delta" rows="6"></textarea>
+												<textarea name="textarea" id="textarea" placeholder="Mensagem" rows="6"></textarea>
 											</div>
 											<!-- Break -->
 											<div class="col-12">
@@ -140,3 +142,47 @@
 			<script src="assets/js/galeria1.js"></script>
 	</body>
 </html>
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer;
+use PHPMailer\SMTP;
+use PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'bodyboard.ismai@gmail.com';                     // SMTP username
+    $mail->Password   = 'Admin123=';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('bodyboard.ismai@gmail.com');
+    $mail->addAddress('bodyboard.ismai@gmail.com');     // Add a recipient
+    $mail->addAddress('bodyboard.ismai@gmail.com');               // Name is optional
+
+    // Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'mensagem';
+    $mail->Body    = '';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
